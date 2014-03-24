7ab76de3d24cc169ed4b7733285609e5867626f1
Dmitry Kuznetsov <dk@bayda.ru>
2014-03-25 02:32:46 +0400

    readme

--- src/update-gh-pages.sh
+++ src/update-gh-pages.sh
@@ -4,8 +4,7 @@ echo "Updating Rating on gh-pages…"
 
 grunt build
 
-grunt replace:version
 git add build/*
 git add index.html
-git commit -m "Update Ya Rating"
+git commit -m "One more try"
 git push
